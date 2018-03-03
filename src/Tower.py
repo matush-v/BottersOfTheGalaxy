2919f6c60432cb6cf5d682a45447e4b536420aa8
Mathurshan Vimalesvaran <mvimalesvaran@hubspot.com>
2018-03-03 11:56:13 -0500

    tower set up"

--- src/Tower.py
+++ src/Tower.py
@@ -2,7 +2,9 @@ import Entity
 
 class Tower(Entity):
 
-    def __init__(self, team, unitId, type, team, attackRange, health, maxHealth, mana, maxMana, attackDamage, movementSpeed, posX, posY):
-        super().__init__(type=)
+    def __init__(self, unitId, team, posX, posY):
+        super(Tower, self).__init__(unitId, type="TOWER", team=team, attackRange=400, health=3000, maxHealth=3000,
+                                    mana=0, maxMana=0, attackDamage=100, movementSpeed=0, posX=posX, posY=posY)
+
 
 
