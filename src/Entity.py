f4e35df500c7081a74cf7309178074b28ec62a84
Mathurshan Vimalesvaran <mvimalesvaran@hubspot.com>
2018-03-03 11:11:44 -0500

    sexy dir updates

---
+++ src/Entity.py
@@ -0,0 +1,15 @@
+def Class(Object):
+
+    def __init__(self, unitId, type, team, attackRange, health, maxHealth, mana, maxMana, attackDamage, movementSpeed):
+        self.unitId = unitId
+        self.type = type
+        self.team = team
+        self.attackRange = attackRange
+        self.health = health
+        self.maxHealth = maxHealth
+        self.mana = mana
+        self.maxMana = maxMana
+        self.attackDamage = attackDamage
+        self.movementSpeed = movementSpeed
+
+
